034df9e41dd4e46feeef00e5535ea78838036d96
zhai_pro <xin_soft@foxmail.com>
2018-08-19 19:00:33 +0800

    改写代码，使其更容易理解。

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ class GameShortest:
         '''
 
         self.line = line
-        # sta和en 表示开始的状态，结束的状态
-        # sta只有2,4,0 2表示箱子开始位置,4表示人的位置,0表示其他。
-        # en只有1,3,0 1表示墙,3表示箱子结束位置,0表示其他。
-        # 现在只需要把sta状态中的2位置移动到en的3的位置即满足条件
-        self.sta = ''
-        self.en = ''
+        # start和end 表示开始的状态，结束的状态
+        # start只有2,4,0 2表示箱子开始位置,4表示人的位置,0表示其他。
+        # end只有1,3,0 1表示墙,3表示箱子结束位置,0表示其他。
+        # 现在只需要把start状态中的2位置移动到end的3的位置即满足条件
+        self.start = ''
+        self.end = ''
         self.col = col
-        # px, py表示4的位置
+        # px, py表示4(人)的位置
         self.px, self.py = -1, -1
         # paths记录最短路径（可能有多条）
         self.paths = []
@@ -37,7 +37,7 @@ class GameShortest:
 
     def pre(self):
         '''
-        1.获得sta开始状态和en结束状态
+        1.获得start开始状态和end结束状态
         2.获得人的起始位置px,py
         第一关的地图可视化为
         1111111111
@@ -52,22 +52,21 @@ class GameShortest:
         1111111111
         '''
         for pos, enum in enumerate(self.line):
-            cx, cy = pos // 10, pos % 10
             if enum == '4':
-                self.px, self.py = cx, cy
-        # 现在只需要把sta开始的状态中的2位置移动到en的3的位置即满足条件
-        staDic = {'0': '0', '1': '0', '2': '2', '3': '0', '4': '4'}
-        enDic = {'0': '0', '1': '1', '2': '0', '3': '3', '4': '0'}
+                self.px, self.py = pos // self.col, pos % self.col
+        # 现在只需要把start状态中的2位置移动到end状态中的3位置即满足条件
+        start_dict = {'0': '0', '1': '0', '2': '2', '3': '0', '4': '4'}
+        end_dict = {'0': '0', '1': '1', '2': '0', '3': '3', '4': '0'}
         for x in self.line:
-            self.sta += staDic[x]
-            self.en += enDic[x]
+            self.start += start_dict[x]
+            self.end += end_dict[x]
 
-    def is_ok(self, sta):
+    def is_ok(self, start):
         '''
-        sta状态中的2位置移动到en的3的位置。
-        :param sta:
+        start状态中的2(盒子)位置移动到end的3(终点)的位置。
+        :param start:
         '''
-        for s, e in zip(sta, self.en):
+        for s, e in zip(start, self.end):
             if e == '3' and s != '2':
                 return False
         return True
@@ -79,61 +78,57 @@ class GameShortest:
         # 4个方向，小写代表只是人移动，大写表示人推着箱子一起移动
         dirs = [[-1, 0, 'u', 'U'], [1, 0, 'd', 'D'], [0, 1, 'r', 'R'], [0, -1, 'l', 'L']]
         # 把开始的状态进入队列(list模拟)，状态包括字符串表示的当前状态、当前的路径、当前人的位置
-        states = [[self.sta, '', self.px, self.py]]
-        # 访问数组(dict模拟)，访问过的状态（字符串）不再访问
-        visi = {}
-        visi[self.sta] = 1
+        states = [[self.start, '', self.px, self.py]]
+        # 访问集合，访问过的状态(字符串)不再访问
+        visi = set()
+        visi.add(self.start)
 
         s_len = 1000
-        while len(states) > 0:
-            sta, path, px, py = states[0]
-            # 4状态的位置
-            ppos = px * self.col + py
-            states = states[1:]
+        while states:
+            start, path, px, py = states.pop(0)
             if len(path) > s_len:
                 break
             # 保存最短路径到paths中
-            if self.is_ok(sta):
+            if self.is_ok(start):
                 if self.len == -1 or len(path) == self.len:
                     self.paths.append(path)
                     self.len = len(path)
                 continue
+            # 4(人)状态的位置
+            ppos = px * self.col + py
 
             for dir in dirs:
                 cx, cy = px + dir[0], py + dir[1]
-                # 4挨着的状态的位置
+                # 4(人)挨着的状态的位置
                 pos = cx * self.col + cy
                 nx, ny = px + 2 * dir[0], py + 2 * dir[1]
                 # 4挨着挨着的状态的位置
                 npos = nx * self.col + ny
-                if not (nx >= 0 and nx < self.col and ny >= 0 and ny < self.col):
+                if not (0 <= nx < self.col and 0 <= ny < self.col):
                     continue
-                # python中字符串不可更改，于是把字符串变成list更改状态后再转换为字符串
-                if sta[pos] == '2' and sta[npos] == '0' and self.en[npos] != '1':
-                    # 人和箱子一起推动，sta中连着的状态为4 2 0，en中第三个不能为1。推完之后sta变为0 4 2
-                    digits = [int(x) for x in sta]
-                    digits[ppos], digits[pos], digits[npos] = 0, 4, 2
-                    new_sta = ''.join(str(x) for x in digits)
-                    if new_sta not in visi:
-                        visi[new_sta] = 1
-                        states.append([new_sta, path + dir[3], cx, cy])
-                elif sta[pos] == '0' and self.en[pos] != '1':
-                    # 人动箱子不动，sta中连着的状态为4 0，en中第二个不能为1。
-                    digits = [int(x) for x in sta]
-                    digits[ppos], digits[pos] = 0, 4
-                    new_sta = ''.join(str(x) for x in digits)
-                    if new_sta not in visi:
-                        visi[new_sta] = 1
-                        states.append([new_sta, path + dir[2], cx, cy])
+                if start[pos] == '2' and start[npos] == '0' and self.end[npos] != '1':
+                    # 人和箱子一起推动，start中连着的状态为4 2 0，end中第三个不能为1。推完之后start变为0 4 2
+                    # python中字符串不可更改，于是把字符串变成list更改状态后再转换为字符串
+                    digits = list(start)
+                    digits[ppos], digits[pos], digits[npos] = '0', '4', '2'
+                    new_start = ''.join(digits)
+                    if new_start not in visi:
+                        visi.add(new_start)
+                        states.append([new_start, path + dir[3], cx, cy])
+                elif start[pos] == '0' and self.end[pos] != '1':
+                    # 人动箱子不动，start中连着的状态为4 0，end中第二个不能为1。
+                    digits = list(start)
+                    digits[ppos], digits[pos] = '0', '4'
+                    new_start = ''.join(digits)
+                    if new_start not in visi:
+                        visi.add(new_start)
+                        states.append([new_start, path + dir[2], cx, cy])
 
 
 if __name__ == '__main__':
     f = open(level_file_path)
-    while(1):
-        line = f.readline()
+    for line in f:
         line = line.strip('\n')
-        if len(line) == 0:
-            break
         gs = GameShortest(line)
 
 '''
